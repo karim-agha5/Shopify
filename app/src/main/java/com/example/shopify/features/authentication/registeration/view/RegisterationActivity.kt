c018feff829fdad17ef21a0309032918ea0172b0
Ahmed Ashraf <ahmed.ashraf.offical@gmail.com>
2023-06-08 00:21:38 +0300

    first-try-signup
    basic ui of signup

---
+++ app/src/main/java/com/example/shopify/features/authentication/registeration/view/RegisterationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shopify.features.authentication.registeration.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shopify.R
+
+class RegisterationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registeration)
+    }
+}
