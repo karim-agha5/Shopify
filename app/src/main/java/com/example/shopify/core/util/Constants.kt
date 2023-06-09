2d6a686de5d36d0a7afa59e75b5c4d377e9f99dc
MohamedEssam <121412932+MohamedEssamAbdu-allah@users.noreply.github.com>
2023-06-09 21:38:24 +0300

    modified home screen UI and constraints

--- app/src/main/java/com/example/shopify/core/util/Constants.kt
+++ app/src/main/java/com/example/shopify/core/util/Constants.kt
@@ -2,6 +2,7 @@ package com.example.shopify.core.util
 
 object Constants {
     //https://{apikey}:{password}@{hostname}/admin/api/{version}/{resource}.json
+    //https://65464e9a325676b2a005d0e7269625bb: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@mad43-alex-and3.myshopify.com/admin/api/2023-04/smart_collections.json
     const val apiKey = "https://65464e9a325676b2a005d0e7269625bb"
     const val password = "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"
 
