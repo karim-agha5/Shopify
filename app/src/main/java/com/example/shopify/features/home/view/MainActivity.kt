78c778f9fc729709cc18704e8a40b2f3cd5843ff
MohamedEssam <121412932+MohamedEssamAbdu-allah@users.noreply.github.com>
2023-06-10 04:05:43 +0300

    added bottom navigation bar and modified brand card

--- app/src/main/java/com/example/shopify/features/home/view/MainActivity.kt
+++ app/src/main/java/com/example/shopify/features/home/view/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.shopify.features.home.view
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import android.view.ViewGroup.MarginLayoutParams
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.marginTop
 import androidx.core.view.updateLayoutParams
-import androidx.fragment.app.FragmentContainerView
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.shopify.R
 import com.example.shopify.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,13 +39,44 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         navController.navigate(R.id.action_splashFragment_to_onboardingFragment2)
 */
-
+        setupNav()
 
     }
 
     private fun initUI(){
         view = findViewById(R.id.view)
+
     }
+
+            private fun setupNav() {
+            val navController = findNavController(R.id.fragment_container_view)
+            findViewById<BottomNavigationView>(R.id.nav_view)
+                .setupWithNavController(navController)
+
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.navigation_home -> showBottomNav()
+                    R.id.navigation_categories -> showBottomNav()
+                    R.id.navigation_me -> showBottomNav()
+                    else -> {
+                        hideBottomNav()
+                    }
+                }
+            }
+        }
+
+        private fun showBottomNav() {
+            binding.navView.visibility = View.VISIBLE
+            binding.navView.alpha = 0.0f
+            binding.navView.animate()
+                .translationY(0f)
+                .alpha(1.0f)
+                .duration = 1000
+        }
+
+        private fun hideBottomNav() {
+            binding.navView.visibility = View.GONE
+        }
 
     private fun adjustContentInsets(){
         ViewCompat.setOnApplyWindowInsetsListener(view) { view, windowInsets ->
