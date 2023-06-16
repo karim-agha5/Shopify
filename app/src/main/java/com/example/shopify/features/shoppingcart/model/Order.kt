25a3cdead09e401c3c6dda7f8b33473d1d8d2da9
karim <karim_agha5@hotmail.com>
2023-06-16 17:38:23 +0300

    add shopping cart ui

---
+++ app/src/main/java/com/example/shopify/features/shoppingcart/model/Order.kt
@@ -0,0 +1,8 @@
+package com.example.shopify.features.shoppingcart.model
+
+data class Order(
+    val orderItemName: String,
+    val orderItemColor: String,
+    val orderItemSize: String,
+    val orderItemPrice: String
+)
