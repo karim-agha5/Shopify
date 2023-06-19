406602c66a693bb49fed1e7c6241c7f3ec759bb2
Ahmed Ashraf <ahmed.ashraf.offical@gmail.com>
2023-06-19 19:36:13 +0300

    bind-rv-with-slider

--- app/src/main/java/com/example/shopify/features/product_details/view/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/shopify/features/product_details/view/ProductDetailsFragment.kt
@@ -18,12 +18,31 @@ import com.example.shopify.core.common.data.model.Product
 import com.example.shopify.core.common.data.model.Variant
 import com.example.shopify.databinding.FragmentProductDetailsBinding
 import com.example.shopify.features.MainActivity
+import com.example.shopify.features.product_details.interfaces.OnImageCardClickListener
 
 
-class ProductDetailsFragment : Fragment() {
+class ProductDetailsFragment : Fragment(), OnImageCardClickListener {
     private val TAG = "ProductDetailsFragment"
     private lateinit var binding: FragmentProductDetailsBinding
     private lateinit var productImagesAdapter: ProductImagesAdapter
+    private var product = Product(
+        id = 12345,
+        image = ImageX(4,4,4,4,"https://cdn.shopify.com/s/files/1/0769/7635/7695/products/7883dc186e15bf29dad696e1e989e914.jpg?v=1685529312","dfa", listOf(),311),
+        images = listOf(
+            ImageX(4,4,4,4,"https://cdn.shopify.com/s/files/1/0769/7635/7695/products/7883dc186e15bf29dad696e1e989e914.jpg?v=1685529312","dfa", listOf(),311),
+            ImageX(4,4,4,4,"https://cdn.shopify.com/s/files/1/0769/7635/7695/products/8cd561824439482e3cea5ba8e3a6e2f6.jpg?v=1685529312","dfa", listOf(),311),
+            ImageX(4,4,4,4,"https://cdn.shopify.com/s/files/1/0769/7635/7695/products/2e1f72987692d2dcc3c02be2f194d6c5.jpg?v=1685529312","dfa", listOf(),311),
+            ImageX(4,4,4,4,"https://cdn.shopify.com/s/files/1/0769/7635/7695/products/6216e82660d881e6f2b0e46dc3f8844a.jpg?v=1685529312","dfa", listOf(),311)
+        ),
+        options = listOf(),
+        product_type = "Clothing",
+        status = "Available",
+        tags = "shirt, summer, casual",
+        title = "ADIDAS | KID'S STAN SMITH",
+        variants = listOf(),
+        vendor = "ADIDAS",
+        isFav = false
+    )
     private var imgs: MutableList<SlideModel> = mutableListOf()
 
     override fun onCreateView(
@@ -32,17 +51,28 @@ class ProductDetailsFragment : Fragment() {
     ): View? {
         binding = FragmentProductDetailsBinding.inflate(inflater)
 
-        imgs.add(SlideModel(R.drawable.ad_image_1_sale))
+        product.images.forEachIndexed { index, image ->
+            imgs.add(SlideModel(product.images[index].src))
+        }
+
+        /*imgs.add(SlideModel(R.drawable.ad_image_1_sale))
         imgs.add(SlideModel(R.drawable.ad_image_2_coupon))
         imgs.add(SlideModel(R.drawable.ad_image_3_coupon))
-        imgs.add(SlideModel(R.drawable.ad_image_4_coupon))
+        imgs.add(SlideModel(R.drawable.ad_image_4_coupon))*/
         binding.imageSlider.setImageList(imgs,ScaleTypes.FIT)
         binding.imageSlider.setSlideAnimation(AnimationTypes.DEPTH_SLIDE)
 
+        binding.imageSlider.setItemChangeListener(object : ItemChangeListener {
+            override fun onItemChanged(position: Int) {
+                Log.d(TAG, "onItemChanged: $position")
+                if(position == cardIndex){
+                    binding.imageSlider.stopSliding()
+                }
+            }
+        })
 
-        productImagesAdapter = ProductImagesAdapter()
+        productImagesAdapter = ProductImagesAdapter(requireContext(), product.images,this)
         binding.rvImages.adapter = productImagesAdapter
-        productImagesAdapter.setProducts(mutableListOf())
 
         return binding.root
     }
@@ -61,7 +91,8 @@ class ProductDetailsFragment : Fragment() {
                 }
             })
         }*/
-        productImagesAdapter.setProducts(mutableListOf(Product(
+
+        /*productImagesAdapter.setProducts(mutableListOf(Product(
             id = 12345,
             image = ImageX(4,4,4,4,"fda","dfa", listOf(),311),
             images = listOf(),
@@ -153,7 +184,7 @@ class ProductDetailsFragment : Fragment() {
                 isFav = false
             ),
         )
-        )
+        )*/
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -163,4 +194,13 @@ class ProductDetailsFragment : Fragment() {
         (activity as MainActivity).binding.navView.visibility = View.GONE
         (activity as MainActivity).binding.toolbar.setNavigationIcon(R.drawable.baseline_back_arrow_24)
     }
+
+    override var cardIndex: Int = 0
+        get() = field
+        set(value) {
+            binding.imageSlider.startSliding()
+            field = value
+        }
+
+
 }
--- app/src/main/java/com/example/shopify/features/product_details/view/ProductImagesAdapter.kt
+++ app/src/main/java/com/example/shopify/features/product_details/view/ProductImagesAdapter.kt
@@ -1,17 +1,28 @@
 package com.example.shopify.features.product_details.view
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.shopify.R
+import com.example.shopify.core.common.data.model.ImageX
 import com.example.shopify.core.common.data.model.Product
 import com.example.shopify.databinding.ProductDetailsImageCardBinding
+import com.example.shopify.features.product_details.interfaces.OnImageCardClickListener
 
-class ProductImagesAdapter(private var productList: MutableList<Product> = mutableListOf()) : RecyclerView.Adapter<ProductImagesAdapter.ViewHolder>() {
+class ProductImagesAdapter(
+    private var context: Context,
+    private var productImgs: List<ImageX>,
+    private var onImageCardClickListener: OnImageCardClickListener,
+    private var productList: MutableList<Product> = mutableListOf()
+) : RecyclerView.Adapter<ProductImagesAdapter.ViewHolder>() {
     private val TAG = "ProductImagesAdapter"
     private var selectedImage = 0
 
@@ -31,7 +42,6 @@ class ProductImagesAdapter(private var productList: MutableList<Product> = mutab
 
     override fun onBindViewHolder(holder: ViewHolder, @SuppressLint("RecyclerView") position: Int) {
         Log.d(TAG, "onBindViewHolder: $selectedImage")
-        val currentProduct = productList[position]
 
         if (selectedImage == position) {
             Log.d(TAG, "onBindViewHolder: up")
@@ -43,23 +53,33 @@ class ProductImagesAdapter(private var productList: MutableList<Product> = mutab
                 ContextCompat.getColor(holder.productCardBinding.root.context, R.color.textGray)
         }
 
-        holder.productCardBinding.mvProductImage.setImageResource(R.drawable.bag)
+//        holder.productCardBinding.mvProductImage.setImageResource(R.drawable.bag)
+        Glide.with(context)
+            .load(productImgs[position].src)
+            .diskCacheStrategy(DiskCacheStrategy.ALL) // Cache image for better performance
+            .placeholder(R.mipmap.placeholder)
+            .error(R.mipmap.placeholder)
+            .into(holder.productCardBinding.mvProductImage)
+
+
         holder.productCardBinding.productDetailsImageCard.setOnClickListener {
             if (position != selectedImage) {
                 selectedImage = position
                 notifyDataSetChanged()
                 Log.d(TAG, "onBindViewHolder: here++")
             }
+//            onImageCardClickListener.changeImage(position)
+            onImageCardClickListener.cardIndex=  position
         }
     }
 
     override fun getItemCount(): Int {
         // Return the size of the list of products
-        return productList.size
+        return productImgs.size
     }
 
-    fun setProducts(products: MutableList<Product>) {
-        productList = products
+    /*fun setProducts(products: MutableList<Product>) {
+        productImgs = products
         notifyDataSetChanged()
-    }
+    }*/
 }
